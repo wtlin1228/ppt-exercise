6deb078f927e1825e86d4047fce35f5323f59aac
林暐唐 <wtlin1228@mini.local>
2020-08-11 16:37:56 +0800

    Updates

---
+++ precache-manifest.73dd231a08a0072c1eb5b5a9a1dde461.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f005af2e7aba3e6963165b3fd23a25c0",
+    "url": "/ppt-exercise/index.html"
+  },
+  {
+    "revision": "554e9fb24f01165a74b4",
+    "url": "/ppt-exercise/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "39fe5fca3f67d5db407d",
+    "url": "/ppt-exercise/static/js/2.a561fac1.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/ppt-exercise/static/js/2.a561fac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "554e9fb24f01165a74b4",
+    "url": "/ppt-exercise/static/js/main.73e64c65.chunk.js"
+  },
+  {
+    "revision": "462823573ecfb1a4637b",
+    "url": "/ppt-exercise/static/js/runtime-main.989931a7.js"
+  }
+]);
